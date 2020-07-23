fde277c05713224148664fad9585b89c018b9974
vuonga1103 <vuonga1103@yahoo.com>
2020-07-22 23:06:40 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,26 @@
 class EmployeesController < ApplicationController
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    
+    if @employee.update(employee_params)
+      redirect_to employee_path(@employee)
+    else 
+      render :edit
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
 end
+
